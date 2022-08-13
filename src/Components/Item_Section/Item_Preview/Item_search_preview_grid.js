37a3e503d04f59dccdc49d521a5fbebdb0ed9b86
Anup Kumar Singh <88195114+akm005@users.noreply.github.com>
2022-08-13 17:15:30 +0530

    minor ui fix

--- src/Components/Item_Section/Item_Preview/Item_search_preview_grid.js
+++ src/Components/Item_Section/Item_Preview/Item_search_preview_grid.js
@@ -33,6 +33,7 @@ export default class ItemSearchPreviewGrid extends Component {
   render() {
     return (
       <>
+<<<<<<< Updated upstream
         {this.props.data.map((res) => (
           <div className="itemview-grid" key={res.sku}>
             <div className="offer-indicator --visible">
@@ -62,6 +63,32 @@ export default class ItemSearchPreviewGrid extends Component {
             </div>
           </div>
         ))}
+=======
+      {this.props.data.map(res => 
+      <div className='itemview-grid' key={res.sku}>
+        <div className='offer-indicator --visible'><span>50%</span></div>
+        <EyeWish product={res} heartclick={this.heartclick} tooggleFav={this.props.tooggleFav}/>
+        <div className='image-container'><img src= {res.images.split("~")[0]} alt='imagepic'/></div>
+        <div className='name-container'>{res.name}</div>
+        <div className='item-detail-box'>
+          <div className='item-detail'>
+            
+            <div className='rating-container'>{star} </div>
+            <div className='price-container'>
+              <span className='current-price'>${res.price*0.5}</span>
+              <span className='original-price'>${res.price}</span>
+              
+            </div>
+          </div>
+          <div className='addtocart-icon'> <AddRemoveItem productId={res.sku}/></div>
+        
+        {/* <AddItem cart={this.props.cart} increseQuantity={this.props.increseQuantity} decreaseQuantity={this.props.decreaseQuantity} product={res} itemid1={res.sku}/> */}
+       
+        
+        </div>
+      </div>
+      )}
+>>>>>>> Stashed changes
       </>
     );
   }
